83df501f7e2ae03863c5d81f078f47c87c6e9b38
EduardsJanitis221RDB271 <eduards.janitis@edu.rtu.lv>
2025-04-08 16:11:17 +0300

    fixes and new content for home

---
+++ Polex/src/Pages/home_components/HeatInsulation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function HeatInsulation(){
+    return (
+        <div>
+            <h1>Efektīva siltumizolācija</h1>
+            <div className="heat-text-container">
+                <h2>Poliuretāna putas nodrošina augstu temperatūras izolāciju,
+                     kas palīdz saglabāt ēku siltu ziemā un vēsu vasarā</h2>
+                <h3>*Poliuretāna putu izcilās temperatūras izolācijas
+                     īpašības novērtēja pat NASA, kur izmantojot tās kosmosa kuģu izolācijā*</h3>
+            </div>
+        </div>
+
+    );
+}
+
+export default HeatInsulation;
