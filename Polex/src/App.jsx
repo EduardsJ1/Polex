f8ba4a536c4ed8e3be819edd7417a0ec463f4705
EduardsJanitis221RDB271 <eduards.janitis@edu.rtu.lv>
2025-04-08 16:09:13 +0300

    fixed suspense

--- Polex/src/App.jsx
+++ Polex/src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from './components/Navbar';
 import './App.css'
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
 import Home from './Pages/Home';
+
 const About = lazy(() => import('./Pages/About'));
 const Contact = lazy(() => import('./Pages/Contact'));
 
@@ -12,14 +12,14 @@ function App() {
     <Router>
       <Navbar />
       <div className="content">
-        <Routes>
-          <Route path="/" element={<React.Suspense fallback={<div>Loading...</div>}><Home /></React.Suspense>} />
-          <Route path="/par-mums" element={<React.Suspense fallback={<div>Loading...</div>}><About /></React.Suspense>} />
-          <Route path="/kontakti" element={<React.Suspense fallback={<div>Loading...</div>}><Contact /></React.Suspense>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/par-mums" element={<Suspense fallback={<div>Loading...</div>}><About /></Suspense>} />
+            <Route path="/kontakti" element={<Suspense fallback={<div>Loading...</div>}><Contact /></Suspense>} />
+          </Routes>
       </div>
     </Router>
   )
 }
 
-export default App
+export default App;
