d5dea7a0d02048aa746908810bcf03c864028f3c
EduardsJanitis221RDB271 <eduards.janitis@edu.rtu.lv>
2025-04-08 16:09:35 +0300

    added new icons

--- Polex/src/Pages/Home.jsx
+++ Polex/src/Pages/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { ReactComponent as SoundMuteIcon } from '../assets/icons/sound_figma_test.svg';
 import { ReactComponent as Icon } from '../assets/icons/Sound_mute.svg';
 import { ReactComponent as SoundIcon } from '../components/icons/soundIcon.jsx';
+import { ReactComponent as DropletIcon } from '../components/icons/dropletIcon.jsx';
+import { ReactComponent as HourglassIcon } from '../components/icons/hourglassIcon.jsx';
+import { ReactComponent as CompactIcon } from '../components/icons/compactIcon.jsx';
+import { ReactComponent as ShieldIcon } from '../components/icons/shieldIcon.jsx';
+import { ReactComponent as StickyIcon } from '../components/icons/stickyIcon.jsx';
+import HeatInsulation from './home_components/HeatInsulation.jsx';
 function Home() {
     const viewBox = "-2 -2 24 24";
 
@@ -30,7 +36,9 @@ function Home() {
                 </div>
 
                 <div className='icon-container'>
-                    <div className='icon'><SoundMuteIcon className='icon-svg' width="" height="" shape-rendering="geometricPrecision" viewBox={viewBox}/></div>
+                    <div className='icon'>
+                        <DropletIcon/>
+                    </div>
                     <div className='icon-text'>
                         <h2>Ūdensizsturīgs</h2>
                         <p>Poliuretāna putas ir ūdensizturīgas un novērš mitruma iekļūšanu ēkā, kas pasargā no pelējuma veidošanās</p>
@@ -38,7 +46,7 @@ function Home() {
                 </div>
 
                 <div className='icon-container'>
-                    <div className='icon'><SoundMuteIcon className='icon-svg' width="" height="" shape-rendering="geometricPrecision" viewBox={viewBox}/></div>
+                    <div className='icon'><HourglassIcon width="100" height="100"/></div>
                     <div className='icon-text'>
                         <h2>Ilgmūžība</h2>
                         <p>Poliuretāna putas ir izturīgas un saglabā savas siltumizolācijas īpašības daudzus gadus</p>
@@ -46,7 +54,7 @@ function Home() {
                 </div>
 
                 <div className='icon-container'>
-                    <div className='icon'><SoundMuteIcon className='icon-svg' /></div>
+                    <div className='icon'><CompactIcon /></div>
                     <div className='icon-text'>
                         <h2>Kompakta izolācija</h2>
                         <p>Poliuretāna putas nodrošina lielisku siltumizolāciju pat plānā kārtā un ir piemērots arī šaurās vietās</p>
@@ -54,7 +62,7 @@ function Home() {
                 </div>
 
                 <div className='icon-container'>
-                    <div className='icon'><SoundMuteIcon className='icon-svg' /></div>
+                    <div className='icon'><StickyIcon /></div>
                     <div className='icon-text'>
                         <h2>Piemērots dažādām virsmām</h2>
                         <p>Poliuretāna putas labi pieķeras pie dažādām virsmām, tostarp kokam, metālam un betonam</p>
@@ -62,7 +70,7 @@ function Home() {
                 </div>
 
                 <div className='icon-container'>
-                    <div className='icon'><SoundMuteIcon className='icon-svg' width="" height="" shape-rendering="geometricPrecision" viewBox={viewBox}/></div>
+                    <div className='icon'><ShieldIcon width="80" height="80"/></div>
                     <div className='icon-text'>
                         <h2>Struktūras izturība</h2>
                         <p>Slēgto šūnu poliuretāna putas stiprina struktūru, uzlabojot ēkas stabilitāti un izturību pret laikapstākļiem</p>
@@ -70,6 +78,7 @@ function Home() {
                 </div>
             </div>
         </div>
+        <HeatInsulation />
     </div>
 
   );
---
+++ Polex/src/components/icons/hourglassIcon.jsx
@@ -0,0 +1,85 @@
+import * as React from "react"
+const SvgComponent = (props) => (
+  <svg
+    // width={24}
+    // height={38}
+    viewBox="0 0 24 38"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g id="hourglass">
+      <g id="base">
+        <rect
+          id="Rectangle 3"
+          x={0.5}
+          y={35.5}
+          width={23}
+          height={2}
+          rx={1}
+          stroke="black"
+        />
+        <rect
+          id="Rectangle 4"
+          x={0.5}
+          y={0.5}
+          width={23}
+          height={2}
+          rx={1}
+          stroke="black"
+        />
+        <path
+          id="Vector 3"
+          d="M11 20C11 20 5 20.5 4 26C3 31.5 3 35 3 35"
+          stroke="black"
+        />
+        <path
+          id="Vector 4"
+          d="M11 18C11 18 5 17.5 4 12C3 6.5 3 3 3 3"
+          stroke="black"
+        />
+        <path
+          id="Vector 5"
+          d="M13 20C13 20 19 20.5 20 26C21 31.5 21 35 21 35"
+          stroke="black"
+        />
+        <path
+          id="Vector 6"
+          d="M13 18C13 18 19 17.5 20 12C21 6.5 21 3 21 3"
+          stroke="black"
+        />
+        <path
+          id="Vector 7"
+          d="M13 18V20"
+          stroke="black"
+          strokeLinecap="round"
+        />
+        <path
+          id="Vector 8"
+          d="M11 18V20"
+          stroke="black"
+          strokeLinecap="round"
+        />
+      </g>
+      <g id="bottom">
+        <path
+          id="Rectangle 6"
+          d="M18.1878 30.9727C18.9858 32.3058 18.0254 34 16.4717 34H7.52826C5.97457 34 5.01421 32.3058 5.81224 30.9727L5.93003 30.776C6.16044 30.3911 6.51356 30.0948 6.93264 29.9348L11.2866 28.2724C11.746 28.097 12.254 28.097 12.7134 28.2724L17.0674 29.9348C17.4864 30.0948 17.8396 30.3911 18.07 30.776L18.1878 30.9727Z"
+          fill="black"
+        />
+      </g>
+      <g id="top">
+        <path
+          id="Rectangle 5"
+          d="M6.70856 14.99C6.01319 13.7731 7.02163 12.2888 8.40911 12.487L11.7172 12.9596C11.9048 12.9864 12.0952 12.9864 12.2828 12.9596L15.5909 12.487C16.9784 12.2888 17.9868 13.7731 17.2914 14.99V14.99C17.1027 15.3202 16.8078 15.5769 16.4546 15.7182L12.7428 17.2029C12.266 17.3936 11.734 17.3936 11.2572 17.2029L7.5454 15.7182C7.19223 15.5769 6.89728 15.3202 6.70856 14.99V14.99Z"
+          fill="black"
+        />
+      </g>
+      <g id="drops">
+        <path id="Line 16" d="M12 21V22" stroke="black" strokeLinecap="round" />
+        <path id="Line 17" d="M12 25V26" stroke="black" strokeLinecap="round" />
+      </g>
+    </g>
+  </svg>
+)
+export { SvgComponent as ReactComponent }
