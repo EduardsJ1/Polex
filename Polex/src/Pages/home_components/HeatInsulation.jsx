52994d917409665071371d4b4ce8fecfc452c1aa
Eduards <eduards.janitis@edu.rtu.lv>
2025-04-09 17:16:35 +0300

    added video for testing

--- Polex/src/Pages/home_components/HeatInsulation.jsx
+++ Polex/src/Pages/home_components/HeatInsulation.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import videoTest from '../../assets/videoTest.mp4';
 function HeatInsulation(){
     return (
         <div>
             <h1>Efektīva siltumizolācija</h1>
-            <div className="heat-text-container">
-                <h2>Poliuretāna putas nodrošina augstu temperatūras izolāciju,
-                     kas palīdz saglabāt ēku siltu ziemā un vēsu vasarā</h2>
-                <h3>*Poliuretāna putu izcilās temperatūras izolācijas
-                     īpašības novērtēja pat NASA, kur izmantojot tās kosmosa kuģu izolācijā*</h3>
+            <div>
+                <div className="heat-text-container">
+                    <h2>Poliuretāna putas nodrošina augstu temperatūras izolāciju,
+                        kas palīdz saglabāt ēku siltu ziemā un vēsu vasarā</h2>
+                    <h4 className='fact-text'>*Poliuretāna putu izcilās temperatūras izolācijas
+                        īpašības novērtēja pat NASA, kur izmantojot tās kosmosa kuģu izolācijā*</h4>
+                </div>
+                <div className='video-container'>
+                    <video className="video" autoPlay loop muted playsInline>
+                        <source src={videoTest} type="video/mp4" />
+                    </video>
+                </div>
             </div>
         </div>
 
