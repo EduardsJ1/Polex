9647ab3266ad78023768c2455d6785dcfa5a40b8
Eduards <eduards.janitis@edu.rtu.lv>
2025-04-12 21:53:29 +0300

    added about page

--- Polex/src/Pages/About.jsx
+++ Polex/src/Pages/About.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import './About.css';
 function About() {
   return (
-    <div className="page">
-      <h1>About Page</h1>
-      <p>This is the about page of our application.</p>
-      <p>Here you can learn more about us and our services.</p>
+    <div className='about-page-main'>
+      <div className='about-us-container'>
+        <h1>Par mums</h1>
+        <p>
+          <span>SIA Polex</span> ir Latvijas uzņēmums, kas specializējas poliuretānu putu siltināšanas pakalpojumu sniegšanā gan privātiem, gan komerciāliem klientiem.
+          Mūsu mērķis ir nodrošināt ilgtspējīgu un efektīvu siltināšanas risinājumus, kas palīdz ietaupīt enerģiju un uzlabot ēkas komfortu.
+        </p>
+      </div>
+      <div className='about-us-container'>
+        <h1>Kas mēs esam?</h1>
+        <p><span>SIA Polex</span> ir Valmierā dibināts uzņēmums, kas sniedz siltināšanas pakalpojumus visā Latvijā</p>
+      </div>
+      <div className='about-us-container'>
+        <h1>Ko mēs piedāvājam?</h1>
+        <ul>
+          <li>Siltināšanu ar slēgto un atvērto poliuretāna šūnu putām</li>
+          <li>Primātmāju, angāru, noliktavu un citu ēku siltināšanu</li>
+          <li>Konsultāciju un atsaucību klientu jautājumiem</li>
+        </ul>
+      </div>
+      <div className='about-us-information'>
+        <div className='about-adress'>
+          <h2>Adrese</h2>
+          <p>Valmiera, Georga Apiņa iela 22, LV-4201</p>
+        </div>
+        <div className='about-name'>
+          <h2>Uzņēmuma nosaukums</h2>
+          <p>SIA Polex</p>
+        </div>
+        <div className='about-reg'>
+          <h2>Reģistrācijas numurs</h2>
+          <p>40203481601</p>
+        </div>
+      </div>
     </div>
   );
 }
