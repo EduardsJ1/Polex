e86fd0228dd02a9967dc7fcf9efaf641b014c108
Eduards <eduards.janitis@edu.rtu.lv>
2025-04-13 00:21:34 +0300

    new css for service and new content

--- Polex/src/Pages/home_components/Service.jsx
+++ Polex/src/Pages/home_components/Service.jsx
@@ -1,4 +1,3 @@
-import { ReactComponent as SoundIcon } from '../../components/icons/shieldIcon.jsx';
 import { ReactComponent as RoofIcon } from '../../components/icons/roofIcon.jsx';
 import { ReactComponent as AtticIcon } from '../../components/icons/atticIcon.jsx';
 import { ReactComponent as FoundationIcon } from '../../components/icons/foundationIcon.jsx';
@@ -17,44 +16,84 @@ function Service(){
         <div className='service-icon-container'>
             <div className="title"><h2>Pakalpojumi var iekļaut</h2></div>
             <div className='service-information'>
-                <div className='icon-containter'>
-                    <div className='icon'><RoofIcon/></div>
-                    <div className='icon-text'><h3>Jumta siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><RoofIcon/></div>
+                    <div className='service-title'><h3>Jumta siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Jumtu siltināšana tiek veikta ar slēgto šūnu poliuretāna putām, kas nodrošina lielisku aizsardzību pret siltuma zudumiem, mitrumu un vēja ietekmi.
+                            Putas cieši pieguļ visām virsmām, tostarp sarežģītiem jumta slīpumiem. Šāda siltināšama ievērojami pagarina jumta kalpošanas laiku un samazina apkures izmaksas.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><AtticIcon/></div>
-                    <div className='icon-text'><h3>Bēniņu siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><AtticIcon/></div>
+                    <div className='service-title'><h3>Bēniņu siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Bēniņu siltināšanā parasti tiek izmantotas atvērto šūnu poliuretāna putas, kas ir elpojošas, vieglas un nodrošina labu skaņas izolāciju.
+                            Šīs putas ir lieliski piemērotas dzīvojamiem bēniņiem. Ja nepieciešama augstāka mitruma aizsardzība, piemērām, ventelācijas trūkuma gadījumā, izmantojam arī slēgtu šūnu putas uz ārējām virsmām.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><FoundationIcon/></div>
-                    <div className='icon-text'><h3>Pamatu siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><FoundationIcon/></div>
+                    <div className='service-title'><h3>Pamatu siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Pamatus izolējam ar slēgto šūnu poliuretāna putām, kuras izceļas ar augstu mehānisko izturību un mitruma necaurlaidību. 
+                            Šīs putas nodrošina efektīvu siltuma saglabāšanu un ilgtermiņa aizsardzību pret mitruma ietekmi un ēkas konstrukcijām.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><FloorIcon/></div>
-                    <div className='icon-text'><h3>Grīdas siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><FloorIcon/></div>
+                    <div className='service-title'><h3>Grīdas siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Grīdām izmantojam slēgto šūnu putas, kas nodrošina izcilu siltumizolāciju pat plānā slānī.
+                            Tās ir īpaši piemērotas starpstāvu pārsegumiem, betona plātnēm un zemgrīdas apkures sistēmām, kur nepieciešama gan siltuma, gan megāniskā slodzes noturība.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><BasementIcon/></div>
-                    <div className='icon-text'><h3>Pagrabu siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><BasementIcon/></div>
+                    <div className='service-title'><h3>Pagrabu siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Pagraba telpās izmantojam slēgto šūnu poliuretāna putas, jo tās ir ūdensizturīgas un izveido cieši pieguļošu izolācijas slāni. 
+                            Tas pasargā no mitruma, pelējuma un palīdz nodrošināt stabilu iekštelpu temperatūru visa gada garumā
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><OuterWallIcon/></div>
-                    <div className='icon-text'><h3>Ārsienu siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><OuterWallIcon/></div>
+                    <div className='service-title'><h3>Ārsienu siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Ārsienas izolējam ar slēgto šūnu poliuretāna putām, kas nodrošina nepārtrauktu un cieši noslēgtu siltumizolācijas slāni.
+                            Tās samazina siltuma zudumus un pasargā ēku no vēja, lietus un citiem ārējiem faktoriem. Lieliski piemērots gan koka, gan mūra sienām.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><InnerWallIcon/></div>
-                    <div className='icon-text'><h3>Iekšsienu siltināšana</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><InnerWallIcon/></div>
+                    <div className='service-title'><h3>Iekšsienu siltināšana</h3></div>
+                    <div className='service-description'>
+                        <p>Iekšsienās parasti izmantojam atvērtu šūnu putas, kas efektīvi uzlabo skaņas izolāciju starp telpām. 
+                            Tās ļauj telpām "elpot", vienlaikus saglabājot siltumu un komfortu iekštelpās.
+                        </p>
+                    </div>
                 </div>
 
-                <div className='icon-containter'>
-                    <div className='icon'><OtherIcon/></div>
-                    <div className='icon-text'><h3>Citām vajadzībām</h3></div>
+                <div className='service-card'>
+                    <div className='service-icon'><OtherIcon/></div>
+                    <div className='service-title'><h3>Citām vajadzībām</h3></div>
+                    <div className='service-description'>
+                        <p>Piedāvājam pielāgotus sitlināšanas risinājumus dažādām vajadzībām, piemēram, konteineriem, transportam un citām specifiskām prasībām.
+                             Atkarībā no konstrukcijas un apstākļiem izmantojam gan atvērto, gan slēgto šūnu putas, nodrošinot efektīvu rezultātu.
+                        </p>
+                    </div>
                 </div>
 
             </div>
