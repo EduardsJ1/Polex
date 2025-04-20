c719f57736c71b08d89fda524c2e913abe557574
Eduards <eduards.janitis@edu.rtu.lv>
2025-04-20 03:04:06 +0300

    testing without lazy loaded pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import './App.css'
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import Loading from './components/Loading';
-
-const About = lazy(() => import('./Pages/About'));
-const Contact = lazy(() => import('./Pages/Contact'));
+// import Loading from './components/Loading';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+// const About = lazy(() => import('./Pages/About'));
+// const Contact = lazy(() => import('./Pages/Contact'));
 
 function App() {
   return (
@@ -16,8 +17,10 @@ function App() {
       <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/par-mums" element={<Suspense fallback={<Loading height="100vh" width="100%" />}><About /></Suspense>} />
-            <Route path="/kontakti" element={<Suspense fallback={<Loading height="100vh" width="100%" />}><Contact /></Suspense>} />
+            {/* <Route path="/par-mums" element={<Suspense fallback={<Loading height="100vh" width="100%" />}><About /></Suspense>} />
+            <Route path="/kontakti" element={<Suspense fallback={<Loading height="100vh" width="100%" />}><Contact /></Suspense>} /> */}
+            <Route path="/par-mums" element={<About/>} />
+            <Route path="/kontakti" element={<Contact/>} />
           </Routes>
       </div>
       <Footer />
