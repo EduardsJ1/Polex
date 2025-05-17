bb91f3ded7f5b2bbe365434973274ae0c856b521
Eduards <eduards.janitis@edu.rtu.lv>
2025-05-18 01:40:31 +0300

    fix for opengraph

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,17 +7,6 @@ function Contact() {
     <title>Polex - Kontakti</title>
     <meta name="description" content="Sazinies ar SIA Polex profesionālu siltināšanas pakalpojumu sniedzēju, lai uzzinātu vairāk par poliuretāna putu siltināšanu un mūsu pakalpojumiem." />
     
-    <meta property="og:url" content="https://polex.lv/contact" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Polex - Kontakti" />
-    <meta property="og:description" content="Sazinies ar SIA Polex profesionālu siltināšanas pakalpojumu sniedzēju, lai uzzinātu vairāk par poliuretāna putu siltināšanu un mūsu pakalpojumiem." />
-    <meta property="og:image" content="https://polex.lv/images/logo.webp" />
-
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="Polex - Kontakti" />
-    <meta name="twitter:description" content="Sazinies ar SIA Polex profesionālu siltināšanas pakalpojumu sniedzēju, lai uzzinātu vairāk par poliuretāna putu siltināšanu un mūsu pakalpojumiem." />
-    <meta name="twitter:image" content="https://polex.lv/images/logo.webp" />
-    
     <div className="contact-page">
       <h1>Droši sazinies ar mums!</h1>
       <div className="contact-page-container">
